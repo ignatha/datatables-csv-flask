02338efef8a548a5ae2558e4bb9df159e623d27b
Ignatha Prabowo <prabowoid.2@gmail.com>
2018-10-20 07:41:05 +0700

    Ref 1

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, render_template, url_for
+import csv
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	return render_template('index.html')
+
+@app.route("/api")
+def api():
+	data = {}
+	data_list = []
+	csvs = [row for row in csv.reader(open('pencairan-pedesaan.csv', 'r'))]
+	data['data'] = csvs
+	return jsonify(data)
+
+
+if __name__ == '__main__':
+	app.run(debug=True,host='0.0.0.0')
